31e5febbdbad91ae1fe1e42498c5489e27bc7898
siaoynli <siaoynli@126.com>
2023-08-10 14:54:15 +0800

    feat(服务): 修复getenv 默认值不能为null

--- config/redis.php
+++ config/redis.php
@@ -15,7 +15,7 @@
 return [
     'default' => [
         'host' => getenv('REDIS_HOST','127.0.0.1'),
-        'password' => getenv('REDIS_PASSWORD',null),
+        'password' => getenv('REDIS_PASSWORD',''),
         'port' => getenv('REDIS_PORT',6379),
         'database' => 0,
     ],
